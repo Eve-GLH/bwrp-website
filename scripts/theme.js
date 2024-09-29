0b95291119b6366e71d83d79bada553cefb2c409
EveBookingOnline <eve@bookingonline.co.uk>
2024-09-29 16:50:21 +0100

    toplinks hover effects

--- scripts/theme.js
+++ scripts/theme.js
@@ -3,6 +3,16 @@ $(document).ready(function(){
         accessibility: true,
         adaptiveHeight: true,
         autoplay: true,
-        slidesToShow: 1
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        asNavFor: '.banner__text'
     });
+    $('.banner__text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.banner',
+        dots: false,
+        focusOnSelect: true
+    })
 });
